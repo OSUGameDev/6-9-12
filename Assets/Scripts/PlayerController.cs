efda81ca6497854a00301349ad819edec8e6193e
Nathan127 <32209187+Nathan127@users.noreply.github.com>
2020-02-01 11:50:54 -0800

    Added spinner to player for fun

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,23 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
+    public bool spinner = false;
 
     Vector2 movement;
 
     void Update()
     {
+        //spinner
+        if(spinner == true)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rb.rotation = 0f;
+        }
+
         //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
