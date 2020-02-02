1905958bb96594d01b3d98b79ee8e37c96456546
Nathan127 <32209187+Nathan127@users.noreply.github.com>
2020-02-01 19:08:03 -0800

    Added Snake Puzzle

---
+++ Assets/Scripts/Snek/Snake.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour
+{
+    public GameObject tailPrefab;
+    public float speed = 0.3f;
+
+    Vector2 dir = Vector2.right;
+    List<Transform> tail = new List<Transform>();
+    bool ate = false;
+    bool playing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Move", speed, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector2.up;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector2.right;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+
+    void Move()
+    {
+        if(playing)
+        {
+            Vector2 v = transform.position;
+
+            transform.Translate(dir);
+
+            if (ate)
+            {
+                GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
+                tail.Insert(0, g.transform);
+                ate = false;
+            }
+            else if (tail.Count > 0)
+            {
+                tail.Last().position = v;
+                tail.Insert(0, tail.Last());
+                tail.RemoveAt(tail.Count - 1);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.name.StartsWith("Snake_Food"))
+        {
+            ate = true;
+            Destroy(coll.gameObject);
+        }
+        else if(coll.gameObject.tag == "Wall")
+        {
+            playing = false;
+            print("You lose");
+        }
+    }
+}
---
+++ Assets/Scripts/Snek/SpawnFood.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    public Transform TopWall;
+    public Transform BottomWall;
+    public Transform LeftWall;
+    public Transform RightWall;
+    public int foodCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Repeat Spawn every 4 seconds, starting 3 seconds in
+        //InvokeRepeating("Spawn", 1, foodCount);
+
+        for (int i = 0; i < foodCount; i++)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        int x = (int)Random.Range(LeftWall.position.x, RightWall.position.x);
+        int y = (int)Random.Range(BottomWall.position.y, TopWall.position.y);
+        Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+}
