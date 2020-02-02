3e962bb0fd9ab66060ff2dad3b95db303c06730e
Nathan127 <32209187+Nathan127@users.noreply.github.com>
2020-02-01 18:09:43 -0800

    Created FlappyBird puzzle

---
+++ Assets/Scripts/Flappy/Bird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed = 2;
+    public float force = 300;
+    public float maxTime = 10;
+    public Rigidbody2D rb;
+
+    float countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        countDown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+
+        if(countDown > 0)
+        {
+            countDown -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("You win!");
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("You lose");
+    }
+}
